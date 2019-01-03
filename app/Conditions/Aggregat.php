71e774d7454987e6d67e9f301c9fe6f9348b77be
Stéphane Bour <sbour@welcome-media.fr>
2019-01-03 22:19:48 +0100

    wip: add aggregat and relative conditions

---
+++ app/Conditions/Aggregat.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Conditions;
+
+class Aggregat
+{
+}
---
+++ app/Conditions/Relative.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Conditions;
+
+class Relative
+{
+    public static function fromRecord(string $condition, array $query, $origin, $values)
+    {
+    }
+}
